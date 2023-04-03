6d90e27513adee2223d2ba7eefe3e91f5bb85955
suijiyuan <suijiyuan@outlook.com>
2023-04-03 19:02:25 +0800

    feat: update project struction

--- app/app.py
+++ app/app.py
@@ -1,12 +1,8 @@
 from flask import Flask
 
-app = Flask(__name__)
 
-
-@app.route('/ping')
-def ping():
-    return 'pong'
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='10001')
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('app.config.setting')
+    app.config.from_object('app.config.secure')
+    return app
