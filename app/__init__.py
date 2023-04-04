f98ed061156a330e8e1220f9872c36cfc6f87acc
suijiyuan <suijiyuan@outlook.com>
2023-04-04 15:51:03 +0800

    feat: optimize setting between different environment

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,6 @@
+import os
+import platform
+
 from flask import Flask
 
 from app.api import register_blueprints_v1
@@ -9,6 +12,22 @@ def register_blueprints(app):
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_pyfile('config/setting.py')
+
+    app.config.from_pyfile('config/config.py')
+
+    system = platform.system()
+    env = os.environ.get('FLASK_ENV')
+
+    if env is None:
+        if system == 'Windows':
+            env = 'development'
+        else:
+            env = 'production'
+
+    if env == 'development':
+        app.config.from_pyfile('config/config_dev.py')
+    elif env == 'production':
+        app.config.from_pyfile('config/config_prod.py')
+
     register_blueprints(app)
     return app
--- app/config/config.py
+++ app/config/config.py
@@ -1,5 +1,4 @@
 import secrets
 
-DEBUG = False
 SECRET_KEY = secrets.token_hex(16)
 DATABASE_URI = 'sqlite:///my_database.db'
